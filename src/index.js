879937d85955e131bb57a2a06c6005739afda7e5
suzannekbach <skbach@uw.edu>
2020-02-05 11:58:03 -0800

    add d3 dependencies and read csv file in js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 
-
-console.log('hello world');
+const d3 = require('d3');
+const csvFile = require('./Burke_Gilman_Bicycle_and_Pedestrian_Counter.csv');
+d3.csv(csvFile).then(function(data) {
+    console.log(data);
+});
