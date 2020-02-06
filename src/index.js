9ffc1011969a4e1bbf61bbd3f00639a69df1391d
Olga Andreeva <andreolg@cs.washington.edu>
2020-02-06 13:35:19 -0800

    Put stuff into functions

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
+(function() {
+	// Make sure the window has loaded before we start trying to 
+	// modify the DOM.
+	window.addEventListener("load", init);
 
-const d3 = require('d3');
-const csvFile = require('./Burke_Gilman_Bicycle_and_Pedestrian_Counter.csv');
-d3.csv(csvFile).then(function(data) {
-    for (i = 0; i < data.length; i++) {
-        // extract the time of day from the date
-        time = data[i].Date.split(" ", 2)[1];
-        data[i].time_of_day = time
-    }
+	 function init() {
+	 	setupSlider();
+	}
 
-    // avg_data is formatted liket this: [{time_of_day: "0:00", 
-    //    ped_north_avg: 2.061206120612061, 
-    //    ped_south_avg: 14.298829882988299, 
-    //    bike_north_avg: 0.5306030603060305, 
-    //    bike_south_avg: 0.3177317731773177
-    //   }, ...]
-    var avg_data = d3.nest()
-    .key(function(d) { return d.time_of_day; })
-    .rollup(function(v) { return { ped_north_avg: d3.mean(v, function (d) { return d['Ped North']; }), 
-                                   ped_south_avg: d3.mean(v, function (d) { return d['Ped South']; }),
-                                   bike_north_avg: d3.mean(v, function (d) { return d['Bike North']; }),
-                                   bike_south_avg: d3.mean(v, function (d) { return d['Bike South']; })
-                                }; 
-                        })
-    .entries(data)
-    .map(function(group) {
-        return {
-          time_of_day: group.key,
-          ped_north_avg: group.value.ped_north_avg,
-          ped_south_avg: group.value.ped_south_avg,
-          bike_north_avg: group.value.bike_north_avg,
-          bike_south_avg: group.value.bike_south_avg
-        }
-      });
-    console.log(avg_data[0]);
-});
+	function setupSlider() {
+		var slider = document.getElementById("myRange");
+		var output = document.getElementById("demo");
+		output.innerHTML = "12:00 PM";
+		
+		slider.oninput = function() {
+			var time = "";
+			if (this.value == 0) {
+				time = "12:00 AM";
+			} else if (this.value < 12) {
+				time = this.value + ":00 AM"
+			} else if (this.value == 12) {
+				time = "12:00 PM";
+			} else { // if (this.value > 12)
+				time = (this.value - 12) + ":00 PM";
+			}
+			output.innerHTML = time;
+		}
+	}
+})();
